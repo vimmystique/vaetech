eef736d77e66dc30f766102de1e19cbd78d09891
vimmystique <vimmystique@gmail.com>
2020-09-24 03:08:00 +0000

    Updates

---
+++ precache-manifest.0ca670a6825b70e8403848492a2936ba.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80bcaa905addadbc2e6aa37d246e871",
+    "url": "/vaetech/index.html"
+  },
+  {
+    "revision": "0d82c89d193a5f1cc570",
+    "url": "/vaetech/static/css/main.0764e5ba.chunk.css"
+  },
+  {
+    "revision": "4bb2355b0d75f0eb52ff",
+    "url": "/vaetech/static/js/2.3745c368.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vaetech/static/js/2.3745c368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d82c89d193a5f1cc570",
+    "url": "/vaetech/static/js/main.2f47e4e2.chunk.js"
+  },
+  {
+    "revision": "330fafbeea4a5a60de3d",
+    "url": "/vaetech/static/js/runtime-main.e880f461.js"
+  },
+  {
+    "revision": "1775932237e3a7b162ea94056bd065b8",
+    "url": "/vaetech/static/media/BebasNeueLight.17759322.woff"
+  },
+  {
+    "revision": "889ef82b567da7360f58e715f8e7b589",
+    "url": "/vaetech/static/media/BebasNeueLight.889ef82b.ttf"
+  },
+  {
+    "revision": "14e7e076f2d519ee6b9f306d1ffde5eb",
+    "url": "/vaetech/static/media/Skranji-Regular.14e7e076.ttf"
+  },
+  {
+    "revision": "700f5bbc4adc49df0f54464a61e5fca3",
+    "url": "/vaetech/static/media/Skranji-Regular.700f5bbc.woff"
+  },
+  {
+    "revision": "146cfc569fd699451283e7ac3406ccd8",
+    "url": "/vaetech/static/media/android.146cfc56.png"
+  },
+  {
+    "revision": "b940131211a5bb4e572f92cec16d890b",
+    "url": "/vaetech/static/media/appdev2.b9401312.png"
+  },
+  {
+    "revision": "c7e770e5efe291c2b02a5e70e8bf980c",
+    "url": "/vaetech/static/media/appsample1.c7e770e5.png"
+  },
+  {
+    "revision": "acf15649ef2a6ce1eb91e55c11459948",
+    "url": "/vaetech/static/media/appsample2.acf15649.png"
+  },
+  {
+    "revision": "d4f2de5257eb0c30b357f5070372c2a7",
+    "url": "/vaetech/static/media/appsample3.d4f2de52.png"
+  },
+  {
+    "revision": "d70c1332f37bb66cc154d53888eb897d",
+    "url": "/vaetech/static/media/barlogo.d70c1332.svg"
+  },
+  {
+    "revision": "7814e2709565631d7a334c15bb8203cc",
+    "url": "/vaetech/static/media/chat.7814e270.svg"
+  },
+  {
+    "revision": "99821c91ce13370ff99ac3b2d4184352",
+    "url": "/vaetech/static/media/coder.99821c91.svg"
+  },
+  {
+    "revision": "fe1512535e6bb81b9ab62fffbd6e4353",
+    "url": "/vaetech/static/media/dm.fe151253.svg"
+  },
+  {
+    "revision": "7f1d61ac1729094a40524f86a00061f6",
+    "url": "/vaetech/static/media/flag.7f1d61ac.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vaetech/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vaetech/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vaetech/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vaetech/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vaetech/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "998dbbd396d2f59ce224269d7dc382cd",
+    "url": "/vaetech/static/media/foot.998dbbd3.svg"
+  },
+  {
+    "revision": "b83aa3c2ed2528a29e6cd5350f807096",
+    "url": "/vaetech/static/media/gh.b83aa3c2.svg"
+  },
+  {
+    "revision": "ca03c2e5a1e92138ba7e8e1f999862bb",
+    "url": "/vaetech/static/media/home.ca03c2e5.svg"
+  },
+  {
+    "revision": "b3b0cd52dfe90c76e4a650db26c28d18",
+    "url": "/vaetech/static/media/ios.b3b0cd52.png"
+  },
+  {
+    "revision": "60d0589d1999dd15c7232b9a9672d960",
+    "url": "/vaetech/static/media/logo.60d0589d.svg"
+  },
+  {
+    "revision": "b3c8d2c01ce1919a8c617a2b185a5ee6",
+    "url": "/vaetech/static/media/mobdev.b3c8d2c0.svg"
+  },
+  {
+    "revision": "842d71621c482f2532f9bebc2bc720fe",
+    "url": "/vaetech/static/media/pwas.842d7162.png"
+  },
+  {
+    "revision": "7e2d32e7141050d758a38b4ec96390c0",
+    "url": "/vaetech/static/media/roboto-light.7e2d32e7.woff"
+  },
+  {
+    "revision": "9ff15bd34ea83e4dd3f23c20c7f5090e",
+    "url": "/vaetech/static/media/roboto-light.9ff15bd3.ttf"
+  },
+  {
+    "revision": "b51c3038dcb05b114138e3644b2f5dca",
+    "url": "/vaetech/static/media/rtvb.b51c3038.svg"
+  },
+  {
+    "revision": "2dea019fa673b44dc2553ddc91f11b5f",
+    "url": "/vaetech/static/media/slider.2dea019f.jpg"
+  },
+  {
+    "revision": "d6bbf526d7096ad86ab978e261e672fb",
+    "url": "/vaetech/static/media/softdev.d6bbf526.svg"
+  },
+  {
+    "revision": "1f5fea9f8e9cdb0280d7f21d5dcae712",
+    "url": "/vaetech/static/media/webdes.1f5fea9f.svg"
+  },
+  {
+    "revision": "38209af9f76de8c0d03942265b9e87cb",
+    "url": "/vaetech/static/media/webdev.38209af9.svg"
+  },
+  {
+    "revision": "10c81203397572e47ea2ae753f305351",
+    "url": "/vaetech/static/media/webdev1.10c81203.png"
+  },
+  {
+    "revision": "265cb40ab44381cbd1cc4efd8db51e01",
+    "url": "/vaetech/static/media/welcomeBtm.265cb40a.svg"
+  }
+]);
